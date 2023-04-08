de64373b4ce3def4ad73bb094814ec50221324ef
BG601@lindenwood.edu <BG601@lindenwood.edu>
2023-04-07 21:27:24 -0500

    Add project files.

---
+++ Test4_3/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test4_3.Models;
+
+
+namespace FTest4_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<DogViewModel> dog = new();
+        public IActionResult Index()
+        {
+
+            // DogViewModel doggo = new DogViewModel();
+            {
+
+                return View();
+            }
+        }
+
+
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dog.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult ManagerData()
+        {
+            DogViewModel doggo = new DogViewModel();
+            return View(dog);
+        }
+
+    }
+}
---
+++ Test4_3/Models/DogViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace Test4_3.Models
+{
+    public class DogViewModel
+    {
+        public string? DogName { get; set; }
+
+        public int Age { get; set; }
+
+        public string? Breed { get; set; }
+
+        public string? CustomerName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public double Zipcode { get; set; }
+    }
+}
